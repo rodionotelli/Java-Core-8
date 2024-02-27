e0e03c35564fcac85d5503ddcf7cea4400cec509
rodionotelli <148154207+rodionotelli@users.noreply.github.com>
2024-02-28 00:21:44 +0300

    Update Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,16 +12,15 @@ public class Server {
         int port = 8080;
 
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
+           try (ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+           {
             System.out.println("New connection accepted");
-
             final String name = in.readLine();
-
             out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+           }
         }
     }
 }
